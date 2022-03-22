64f06c9938f958f9848c48d0caa9c9c2e33f4e6b
Doan Khiet Thanh <81890086+doankhietthanh@users.noreply.github.com>
2022-03-22 15:55:04 +0700

    update responsive

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,9 @@
 const boxes = document.querySelectorAll(".drive__item");
 const header = document.querySelector(".header");
+const menuBtn = document.querySelector(".header__menu-btn span");
+const menuList = document.querySelector(".header__menu-list");
+
+let isMenu = false;
 
 window.addEventListener("scroll", () => {
   // console.log(window.scrollY);
@@ -20,4 +24,19 @@ window.addEventListener("scroll", () => {
       box.classList.remove("show");
     }
   });
+});
+
+// menuBtn.addEventListener("click", (e) => {
+//   menuList.classList.add("show");
+//   isMenu = true;
+// });
+
+window.addEventListener("click", (e) => {
+  if (e.target !== menuList && isMenu) {
+    menuList.classList.remove("show");
+    isMenu = false;
+  } else if (e.target === menuBtn) {
+    menuList.classList.add("show");
+    isMenu = true;
+  }
 });
