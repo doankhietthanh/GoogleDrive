f741bf090674eb733bc92108a0fc72b8cf019205
Doan Khiet Thanh <81890086+doankhietthanh@users.noreply.github.com>
2022-03-23 07:50:21 +0700

    update resources

--- js/animation.js
+++ js/animation.js
@@ -40,32 +40,3 @@ window.addEventListener("click", (e) => {
     isMenu = true;
   }
 });
-
-const cover_scroll = document.querySelector(".cover_scroll");
-const scroll = document.querySelector(".scroll");
-const total = scroll.children.length;
-const CART_WIDTH = 398;
-const MARGIN = 20;
-const prev = document.querySelector(".resources_dri.btn-prev-slider");
-const next = document.querySelector(".resources_dri.btn-next-slider");
-
-let current = 0;
-
-prev.addEventListener("click", (e) => {
-  if (current === 0) {
-    current = (total - 3) * (CART_WIDTH + MARGIN);
-  } else {
-    current -= CART_WIDTH + MARGIN;
-  }
-  scroll.style.transform = `translateX(${-current}px)`;
-});
-
-next.addEventListener("click", (e) => {
-  current += CART_WIDTH + MARGIN;
-  if (current > (total - 3) * (CART_WIDTH + MARGIN)) {
-    current = 0;
-  }
-  scroll.style.transform = `translateX(-${current}px)`;
-});
-
-scroll.style.width = `${total * CART_WIDTH + MARGIN * (total - 1)}px`;
